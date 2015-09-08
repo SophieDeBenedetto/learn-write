1219c960d7b88d4fd1c84b0bafe484424b684cfa
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-09-08 14:38:58 -0400

    fix bug in readme lint so that valid with no code snippets, refactor readme linter class

--- lib/learn_linter/readme_linter.rb
+++ lib/learn_linter/readme_linter.rb
@@ -2,14 +2,30 @@ require 'pry'
 class ReadmeLinter
 
   def self.parse_file(file, learn_error)
-    readme = File.open(file).read.split(" ")
-    readme.each do |chars|
+    file_string = File.open(file).read
+    if has_code_snippets?(file_string)
+      validate_snippets(file_string, learn_error)
+    else
+      green_light(learn_error)
+    end
+  end
+
+  def self.green_light(learn_error)
+    learn_error.readme_error[:valid_readme] = true
+    learn_error.valid_readme = {message: "valid readme", color: :green}
+  end
+
+  def self.has_code_snippets?(file)
+    file.match(/``/)
+  end
+
+  def self.validate_snippets(file, learn_error)
+    file.split(" ").each do |chars|
       if chars.match(/``/)
         if !(chars.match(/```ruby/) || chars.match(/```bash/) || chars.match(/```objc/) || chars.match(/```javascript/) || chars.match(/``` /))
           break
         else
-          learn_error.readme_error[:valid_readme] = true
-          learn_error.valid_readme = {message: "valid readme", color: :green}
+          green_light(learn_error)
         end
       end
     end
